1728776d5c51a65950de281adc5aa20fa585e368
Paul Groudas <me@paulgroudas.com>
2011-11-02 22:12:18 -0400

    PJ: Homework 2 finished

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -19,14 +19,9 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+J =  (1/m) * (log(sigmoid(theta'*X'))*(-y) - log(1 - sigmoid(theta'*X'))*(1 - y));
 
-
-
-
-
-
-
-
+grad = (1/m) * (((sigmoid(theta'*X')) - y')*X)';
 % =============================================================
 
 end
--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,6 +17,18 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+theta_multiplier = eye(length(theta));
+theta_multiplier(1,1) = 0;
+
+regularized_theta = theta_multiplier * theta;
+
+J =  (1/m) * (log(sigmoid(theta'*X'))*(-y) \
+     - log(1 - sigmoid(theta'*X'))*(1 - y)) \
+     + (lambda / (2*m)) *(regularized_theta' * regularized_theta);
+
+grad = (1/m) * (((sigmoid(theta'*X')) - y')*X)';
+grad = grad + (lambda / m) * (theta_multiplier * theta);
+
 
 
 
